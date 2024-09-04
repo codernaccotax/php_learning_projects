c7130f7825d7a919ed0e0ca1f94426f5bcde770e
Coder & AccoTax <codernaccotax@gmail.com>
2024-09-04 10:56:18 +0530

    htmlspecialchars function added

--- php_mcqs/index.php
+++ php_mcqs/index.php
@@ -13,26 +13,26 @@
             <?php
             require 'config.php'; // Include the database configuration file
 
-            $stmt = $pdo->query('SELECT * FROM questions LIMIT 50');
+            $stmt = $pdo->query('SELECT * FROM questions order by RAND() LIMIT 20');
             $i = 1;
             while ($row = $stmt->fetch()) {
                 echo '<div class="mb-3">';
                 echo '<h5>' . $i . '. ' . $row['question'] . '</h5>';
                 echo '<div class="form-check">';
                 echo '<input class="form-check-input" type="radio" name="answer' . $row['id'] . '" value="A" required>';
-                echo '<label class="form-check-label">' . $row['option_a'] . '</label>';
+                echo '<label class="form-check-label">' . htmlspecialchars($row['option_a']) . '</label>';
                 echo '</div>';
                 echo '<div class="form-check">';
                 echo '<input class="form-check-input" type="radio" name="answer' . $row['id'] . '" value="B">';
-                echo '<label class="form-check-label">' . $row['option_b'] . '</label>';
+                echo '<label class="form-check-label">' . htmlspecialchars($row['option_b']) . '</label>';
                 echo '</div>';
                 echo '<div class="form-check">';
                 echo '<input class="form-check-input" type="radio" name="answer' . $row['id'] . '" value="C">';
-                echo '<label class="form-check-label">' . $row['option_c'] . '</label>';
+                echo '<label class="form-check-label">' . htmlspecialchars($row['option_c']) . '</label>';
                 echo '</div>';
                 echo '<div class="form-check">';
                 echo '<input class="form-check-input" type="radio" name="answer' . $row['id'] . '" value="D">';
-                echo '<label class="form-check-label">' . $row['option_d'] . '</label>';
+                echo '<label class="form-check-label">' . htmlspecialchars($row['option_d']) . '</label>';
                 echo '</div>';
                 echo '</div>';
                 $i++;
